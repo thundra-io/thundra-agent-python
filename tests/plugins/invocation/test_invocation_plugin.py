5bd84b5d1b9f1bbb9967f66d71e6a0940faf6c86
fazilet ozer <fazilet@opsgenie.com>
2018-05-17 17:13:37 +0300

    Added new test cases

---
+++ tests/plugins/invocation/test_invocation_plugin.py
@@ -0,0 +1,103 @@
+from thundra.plugins.invocation.invocation_plugin import InvocationPlugin
+
+
+def test_cold_starts(handler_with_apikey, mock_context, mock_event, environment_variables, monkeypatch):
+    e_v = environment_variables
+    e_v.start()
+    monkeypatch.setattr(InvocationPlugin, 'IS_COLD_START', True)
+    thundra, handler = handler_with_apikey
+
+    invocation_plugin = None
+    for plugin in thundra.plugins:
+        if type(plugin) is InvocationPlugin:
+            invocation_plugin = plugin
+
+    handler(mock_event, mock_context)
+    assert invocation_plugin.invocation_data['coldStart'] is True
+
+    handler(mock_event, mock_context)
+    assert invocation_plugin.invocation_data['coldStart'] is False
+    e_v.stop()
+
+
+def test_if_error_is_added_to_report(handler_with_exception, mock_context, mock_event, environment_variables):
+    e_v = environment_variables
+    e_v.start()
+    thundra, handler = handler_with_exception
+
+    invocation_plugin = None
+    for plugin in thundra.plugins:
+        if type(plugin) is InvocationPlugin:
+            invocation_plugin = plugin
+
+    try:
+        handler(mock_event, mock_context)
+    except Exception as e:
+        pass
+
+    assert invocation_plugin.invocation_data['erroneous'] is True
+    assert invocation_plugin.invocation_data['errorType'] == 'Exception'
+    assert invocation_plugin.invocation_data['errorMessage'] == 'hello'
+
+    e_v.stop()
+
+
+def test_report(handler_with_profile, mock_context, mock_event):
+
+    thundra, handler = handler_with_profile
+
+    invocation_plugin = None
+    for plugin in thundra.plugins:
+        if type(plugin) is InvocationPlugin:
+            invocation_plugin = plugin
+
+    handler(mock_event, mock_context)
+
+    assert invocation_plugin.invocation_data['startTimestamp'] is not None
+    assert invocation_plugin.invocation_data['endTimestamp'] is not None
+
+    start_time = invocation_plugin.invocation_data['startTimestamp']
+    end_time = invocation_plugin.invocation_data['endTimestamp']
+
+    duration = end_time - start_time
+    duration_in_ms = int(duration * 1000)
+
+    assert invocation_plugin.invocation_data['duration'] == duration_in_ms
+    assert invocation_plugin.invocation_data['erroneous'] is False
+    assert invocation_plugin.invocation_data['errorType'] == ''
+    assert invocation_plugin.invocation_data['errorMessage'] == ''
+
+    assert invocation_plugin.invocation_data['region'] == 'region'
+    assert invocation_plugin.invocation_data['memorySize'] == 128
+
+
+def test_when_app_profile_exists(handler_with_profile, mock_context, mock_event):
+
+    thundra, handler = handler_with_profile
+
+    invocation_plugin = None
+    for plugin in thundra.plugins:
+        if type(plugin) is InvocationPlugin:
+            invocation_plugin = plugin
+
+    handler(mock_event, mock_context)
+
+    assert invocation_plugin.invocation_data['applicationProfile'] == 'profile'
+
+
+def test_when_app_profile_not_exists(handler_with_apikey, mock_context, mock_event, environment_variables):
+    e_v = environment_variables
+    e_v.start()
+    thundra, handler = handler_with_apikey
+
+    invocation_plugin = None
+    for plugin in thundra.plugins:
+        if type(plugin) is InvocationPlugin:
+            invocation_plugin = plugin
+
+    handler(mock_event, mock_context)
+
+    assert invocation_plugin.invocation_data['applicationProfile'] is ''
+
+    e_v.stop()
+
