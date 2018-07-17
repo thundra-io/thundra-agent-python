cc49a03b26ad2510cb0265190a95a4e17391c210
dsalv <dias.alymbekov@gmail.com>
2018-07-17 18:22:53 +0300

    added automatic instrumentation with environement variables
    Automatic Instrumentation using patcher

--- thundra/thundra_agent.py
+++ thundra/thundra_agent.py
@@ -180,4 +180,8 @@ class Thundra:
         self.execute_hook('after:invocation', self.data)
         self.reporter.send_report()
 
+    def patch(self):
+        sys.meta_path.insert(0, utils.ThundraFinder('businessLogic'))
+
+
 
