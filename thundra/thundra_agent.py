ee2df6c0a15435cd90d09a30b3dcba8a17e520e1
dsalv <dias.alymbekov@gmail.com>
2018-07-19 15:49:39 +0300

    Refactored Patcher
    Added Import Patcher Plugin

--- thundra/thundra_agent.py
+++ thundra/thundra_agent.py
@@ -180,8 +180,5 @@ class Thundra:
         self.execute_hook('after:invocation', self.data)
         self.reporter.send_report()
 
-    def patch(self):
-        sys.meta_path.insert(0, utils.ThundraFinder('businessLogic'))
-
 
 
