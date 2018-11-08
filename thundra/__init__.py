68e3bd3a15f62692c673063d5a843170e18a3ed0
Mustafa Motani <motani@thundra.io>
2018-11-08 17:32:12 +0300

    Added dynamodb sdk

---
+++ thundra/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from importlib import import_module
+import thundra.integrations.modules as integrations
+
+
+def _import_exists(module_name):
+    """
+    Validates if import module exists
+    :param module_name: module name to import
+    :return: Bool
+    """
+    try:
+        import_module(module_name)
+        return True
+    except ImportError:
+        return False
+
+
+for patch_module in integrations.MODULES:
+    if _import_exists(patch_module):
+        integrations.MODULES[patch_module].patch()
---
+++ thundra/integrations/modules/botocore.py
@@ -0,0 +1,32 @@
+"""
+botocore patcher module.
+"""
+
+from __future__ import absolute_import
+import wrapt
+from thundra.integrations.modules.generic_wrapper import wrapper
+from ..events.botocore import AWSEventListeners
+
+
+def _wrapper(wrapped, instance, args, kwargs):
+    """
+    General wrapper for botocore instrumentation.
+    :param wrapped: wrapt's wrapped
+    :param instance: wrapt's instance
+    :param args: wrapt's args
+    :param kwargs: wrapt's kwargs
+    :return: None
+    """
+    return wrapper(AWSEventListeners, wrapped, instance, args, kwargs)
+
+
+def patch():
+    """
+    Patch module.
+    :return: None
+    """
+    wrapt.wrap_function_wrapper(
+        'botocore.client',
+        'BaseClient._make_api_call',
+        _wrapper
+    )
