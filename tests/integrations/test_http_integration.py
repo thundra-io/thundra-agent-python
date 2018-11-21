7ea618f43a574c9fc699308682df3ea12454ef96
Hamit Burak Emre <hamitburakemre@gmail.com>
2018-11-22 00:58:35 +0300

    Add errorneous http call test

--- tests/integrations/test_http_integration.py
+++ tests/integrations/test_http_integration.py
@@ -25,5 +25,37 @@ def test_successful_http_call():
     assert http_span.get_tag(constants.HttpTags['HTTP_HOST']) == host
     assert http_span.get_tag(constants.HttpTags['HTTP_PATH']) == path
     assert http_span.get_tag(constants.HttpTags['QUERY_PARAMS']) == query
+
+    tracer.clear()
+
+def test_errorneous_http_call():
+    url = 'http://adummyurlthatnotexists.xyz/'
+    parsed_url = urlparse(url)
+    path = parsed_url.path
+    query = parsed_url.query
+    host = parsed_url.netloc
+    
+    try:
+        requests.get(url)
+    except Exception:
+        pass
+
+    tracer = ThundraTracer.get_instance()
+    http_span = tracer.recorder.finished_span_stack[0]
+
+    assert http_span.operation_name == url
+    assert http_span.domain_name == constants.DomainNames['API']
+    assert http_span.class_name == constants.ClassNames['HTTP']
+
+    assert http_span.get_tag(constants.SpanTags['SPAN_TYPE']) == constants.SpanTypes['HTTP']
+    assert http_span.get_tag(constants.SpanTags['OPERATION_TYPE']) == 'CALL'
+    assert http_span.get_tag(constants.HttpTags['HTTP_METHOD']) == 'GET'
+    assert http_span.get_tag(constants.HttpTags['HTTP_URL']) == url
+    assert http_span.get_tag(constants.HttpTags['HTTP_HOST']) == host
+    assert http_span.get_tag(constants.HttpTags['HTTP_PATH']) == path
+    assert http_span.get_tag(constants.HttpTags['QUERY_PARAMS']) == query
+    assert http_span.get_tag('error') == True
+
+    tracer.clear()
     
     
