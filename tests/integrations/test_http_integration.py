610dda6a6ab833b276b7ecd795c0c028bd536492
Hamit Burak Emre <hamitburakemre@gmail.com>
2018-11-21 18:50:43 +0300

    Add test for successful http call

---
+++ tests/integrations/test_http_integration.py
@@ -0,0 +1,29 @@
+import requests
+from thundra.opentracing.tracer import ThundraTracer
+from urllib.parse import urlparse
+from thundra import constants
+
+def test_successful_http_call():
+    url = 'https://jsonplaceholder.typicode.com/users/3'
+    parsed_url = urlparse(url)
+    path = parsed_url.path
+    query = parsed_url.query
+    host = parsed_url.netloc
+    
+    requests.get(url)
+    tracer = ThundraTracer.get_instance()
+    http_span = tracer.recorder.finished_span_stack[0]
+
+    assert http_span.operation_name == url
+    assert http_span.domain_name == constants.DomainNames['API']
+    assert http_span.class_name == constants.ClassNames['HTTP']
+
+    assert http_span.get_tag(constants.SpanTags['SPAN_TYPE']) == constants.SpanTypes['HTTP']
+    assert http_span.get_tag(constants.SpanTags['OPERATION_TYPE']) == 'CALL'
+    assert http_span.get_tag(constants.HttpTags['HTTP_METHOD']) == 'GET'
+    assert http_span.get_tag(constants.HttpTags['HTTP_URL']) == url
+    assert http_span.get_tag(constants.HttpTags['HTTP_HOST']) == host
+    assert http_span.get_tag(constants.HttpTags['HTTP_PATH']) == path
+    assert http_span.get_tag(constants.HttpTags['QUERY_PARAMS']) == query
+    
+    
