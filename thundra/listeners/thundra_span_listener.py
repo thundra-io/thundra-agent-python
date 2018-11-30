091bf188386fa53a567742a9b188859925a96c86
Mustafa Motani <motani@thundra.io>
2018-11-30 18:29:50 +0300

    Added missing modules

---
+++ thundra/listeners/thundra_span_listener.py
@@ -0,0 +1,11 @@
+import abc
+
+
+class ThundraSpanListener(abc.ABC):
+
+    @abc.abstractmethod
+    def on_span_started(self, operation_name):
+        raise Exception("should be implemented")
+
+    def on_span_finished(self, span):
+        raise Exception("should be implemented")
