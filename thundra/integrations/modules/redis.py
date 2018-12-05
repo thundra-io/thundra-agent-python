656d2b3049b9f330625375568e959e65a2bf4895
Mustafa Motani <motani@thundra.io>
2018-12-05 14:51:42 +0300

    Removed redis dependency

--- thundra/integrations/modules/redis.py
+++ thundra/integrations/modules/redis.py
@@ -4,7 +4,6 @@ botocore patcher module.
 
 from __future__ import absolute_import
 import wrapt
-import redis
 from thundra import utils
 from thundra import constants
 from thundra.integrations.redis import RedisIntegration
@@ -23,9 +22,10 @@ def _wrapper(wrapped, instance, args, kwargs):
 def patch():
     disable_redis_integration_by_env = utils.get_configuration(constants.THUNDRA_DISABLE_REDIS_INTEGRATION)
     if not utils.should_disable(disable_redis_integration_by_env):
-        methods = [method_name for method_name in dir(redis.client.Redis)
-         if callable(getattr(redis.client.Redis, method_name)) and not method_name.__contains__('_')]
-
+        redis_commands = constants.RedisCommandTypes
+        methods = []
+        for key in redis_commands:
+            methods.append(key.lower())
         for method in methods:
             wrapt.wrap_function_wrapper(
                 'redis.client',
