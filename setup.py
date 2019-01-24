c25f976543755683760ee2dfd80ceb2787017c42
Hamit Burak Emre <hamitburakemre@gmail.com>
2019-01-24 15:02:14 +0300

    Increase agent version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='thundra',
-      version='2.1.0',
+      version='2.1.1',
       description='Thundra Python agent',
       url='https://github.com/thundra-io/thundra-lambda-agent-python',
       author='Thundra',
