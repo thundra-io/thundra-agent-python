16a59f0c3bd8dd328ef08dd4a8c28c5bb21864d7
Hamit Burak Emre <hamitburakemre@gmail.com>
2019-03-07 10:07:46 +0300

    Update test_es_integration.py

--- tests/integrations/test_es_integration.py
+++ tests/integrations/test_es_integration.py
@@ -72,7 +72,8 @@ def test_get_doc():
 def test_refresh():
     try:
         es = Elasticsearch([{'host': 'test', 'port': 3737}], max_retries=0)
-        es.get(index='test-index', doc_type='tweet', id=1)
+        res = es.indices.refresh(index='test-index')
+        print(res)
     except ElasticsearchException as e:
         pass
     finally:
@@ -80,18 +81,18 @@ def test_refresh():
         span = tracer.get_spans()[0]
         tracer.clear()
 
-        assert span.operation_name == '/test-index/tweet/1'
+        assert span.operation_name == '/test-index/_refresh'
         assert span.class_name == constants.ClassNames['ELASTICSEARCH']
         assert span.domain_name == constants.DomainNames['DB']
         
         assert span.get_tag(constants.ESTags['ES_HOSTS']) == ['http://test:3737']
-        assert span.get_tag(constants.ESTags['ES_METHOD']) == 'GET'
-        assert span.get_tag(constants.ESTags['ES_URI']) == '/test-index/tweet/1'
+        assert span.get_tag(constants.ESTags['ES_METHOD']) == 'POST'
+        assert span.get_tag(constants.ESTags['ES_URI']) == '/test-index/_refresh'
         assert span.get_tag(constants.ESTags['ES_BODY']) == {}
 
         assert span.get_tag(constants.DBTags['DB_TYPE']) == 'elasticsearch'
         
-        assert span.get_tag(constants.SpanTags['OPERATION_TYPE']) == 'READ'
+        assert span.get_tag(constants.SpanTags['OPERATION_TYPE']) == 'WRITE'
         assert span.get_tag(constants.SpanTags['TRIGGER_OPERATION_NAMES']) == ['']
         assert span.get_tag(constants.SpanTags['TRIGGER_DOMAIN_NAME']) == constants.LAMBDA_APPLICATION_DOMAIN_NAME
         assert span.get_tag(constants.SpanTags['TRIGGER_CLASS_NAME']) == constants.LAMBDA_APPLICATION_CLASS_NAME
