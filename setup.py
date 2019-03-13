1ea72c2473ef0f36e82c50633e3946d55f6a26f6
Özge Lüle <luleozge@gmail.com>
2019-03-13 09:55:27 +0300

    Version update for new release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='thundra',
-      version='2.2.4',
+      version='2.2.5',
       description='Thundra Python agent',
       url='https://github.com/thundra-io/thundra-lambda-agent-python',
       author='Thundra',
