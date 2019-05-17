b904ef75a14719fd8b59e4a1c73cff1564bb4ccb
Özge Lüle <luleozge@gmail.com>
2019-05-17 11:33:29 +0300

    Fix command listener not exists error

--- thundra/integrations/mongodb.py
+++ thundra/integrations/mongodb.py
@@ -11,7 +11,7 @@ try:
     from pymongo.monitoring import CommandListener
     from bson.json_util import dumps
 except ImportError:
-    pass
+    CommandListener = object
 
 
 logger = logging.getLogger(__name__)
