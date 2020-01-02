1e45c7b6482d0ed866ba46a2e8e9c27a418b7662
Özge Lüle <default-usr@users.noreply.github.com>
2020-01-02 15:23:10 +0300

    Trace callproc method of mysql and pgsql cursor (#113)

--- tests/integrations/test_mysql_integration.py
+++ tests/integrations/test_mysql_integration.py
@@ -102,3 +102,34 @@ def test_mysql_integration_with_empty_query():
 
         tracer.clear()
         connection.close()
+
+
+def test_mysql_integration_callproc():
+    connection = mysql.connector.connect(
+        user='user',
+        host='localhost',
+        password='userpass',
+        database='db', auth_plugin='mysql_native_password')
+
+    try:
+        cursor = connection.cursor()
+        cursor.callproc('multiply')
+    except MySQLError:
+        pass
+    finally:
+        tracer = ThundraTracer.get_instance()
+        mysql_span = tracer.get_spans()[0]
+
+        assert mysql_span.domain_name == constants.DomainNames['DB']
+        assert mysql_span.class_name == constants.ClassNames['MYSQL']
+        assert mysql_span.operation_name == 'db'
+        assert mysql_span.get_tag(constants.SpanTags['OPERATION_TYPE']) == ''
+        assert mysql_span.get_tag(constants.SpanTags['DB_INSTANCE']) == 'db'
+        assert mysql_span.get_tag(constants.SpanTags['DB_HOST']) == 'localhost'
+        assert mysql_span.get_tag(constants.SpanTags['DB_STATEMENT']) == 'multiply'
+        assert mysql_span.get_tag(constants.SpanTags['DB_STATEMENT_TYPE']) == 'MULTIPLY'
+        assert mysql_span.get_tag(constants.SpanTags['TRIGGER_DOMAIN_NAME']) == 'API'
+        assert mysql_span.get_tag(constants.SpanTags['TRIGGER_CLASS_NAME']) == 'AWS-Lambda'
+
+        tracer.clear()
+        connection.close()
--- tests/integrations/test_postgre_integration.py
+++ tests/integrations/test_postgre_integration.py
@@ -102,3 +102,34 @@ def test_postgre_integration_with_empty_query():
 
         tracer.clear()
         connection.close()
+
+
+def test_postgre_integration_callproc():
+    connection =  psycopg2.connect(
+        user='user',
+        host='localhost',
+        password='userpass',
+        dbname='db')
+
+    try:
+        cursor = connection.cursor()
+        cursor.callproc('multiply', (3, 5))
+    except PostgreError:
+        pass
+    finally:
+        tracer = ThundraTracer.get_instance()
+        postgre_span = tracer.get_spans()[0]
+
+        assert postgre_span.domain_name == constants.DomainNames['DB']
+        assert postgre_span.class_name == constants.ClassNames['POSTGRESQL']
+        assert postgre_span.operation_name == 'db'
+        assert postgre_span.get_tag(constants.SpanTags['OPERATION_TYPE']) == ''
+        assert postgre_span.get_tag(constants.SpanTags['DB_INSTANCE']) == 'db'
+        assert postgre_span.get_tag(constants.SpanTags['DB_HOST']) == 'localhost'
+        assert postgre_span.get_tag(constants.SpanTags['DB_STATEMENT']) == 'multiply'
+        assert postgre_span.get_tag(constants.SpanTags['DB_STATEMENT_TYPE']) == 'MULTIPLY'
+        assert postgre_span.get_tag(constants.SpanTags['TRIGGER_DOMAIN_NAME']) == 'API'
+        assert postgre_span.get_tag(constants.SpanTags['TRIGGER_CLASS_NAME']) == 'AWS-Lambda'
+
+        tracer.clear()
+        connection.close()
