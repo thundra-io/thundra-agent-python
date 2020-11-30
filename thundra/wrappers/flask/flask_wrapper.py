1dce6f7037ae31b2627339a32b9d37d63519c712
Özge Lüle <luleozge@gmail.com>
2020-11-30 11:18:23 +0300

    Refactor

--- thundra/wrappers/flask/flask_wrapper.py
+++ thundra/wrappers/flask/flask_wrapper.py
@@ -13,6 +13,12 @@ from thundra.wrappers import wrapper_utils, web_wrapper_utils
 from thundra.wrappers.base_wrapper import BaseWrapper
 from thundra.wrappers.flask import flask_executor
 
+try:
+    from flask import request, g
+except ImportError:
+    request = None
+    g = None
+
 logger = logging.getLogger(__name__)
 
 
@@ -33,26 +39,23 @@ class FlaskWrapper(BaseWrapper):
         web_wrapper_utils.update_application_info(self.application_info_provider, self.plugin_context.application_info,
                                                   constants.ClassNames['FLASK'])
 
-    def before_request(self, request):
+    def before_request(self, _request):
         # Execution context initialization
         execution_context = wrapper_utils.create_execution_context()
-        execution_context.platform_data['request'] = request
+        execution_context.platform_data['request'] = _request
 
         # Execute plugin hooks before running user's handler
         self.plugin_context.request_count += 1
         self.execute_hook('before:invocation', execution_context)
 
-        try:
-            from flask import g
+        if g is not None:
             g.thundra_execution_context = execution_context
-        except ImportError:
-            pass
+
         return execution_context
 
     def after_request(self, response):
         try:
-            from flask import g
-            if hasattr(g, 'thundra_execution_context'):
+            if g is not None and hasattr(g, 'thundra_execution_context'):
                 execution_context = g.thundra_execution_context
                 if response:
                     execution_context.response = response
@@ -62,8 +65,7 @@ class FlaskWrapper(BaseWrapper):
 
     def teardown_request(self, exception=None):
         try:
-            from flask import g
-            if hasattr(g, 'thundra_execution_context'):
+            if g is not None and hasattr(g, 'thundra_execution_context'):
                 execution_context = g.thundra_execution_context
                 if exception:
                     execution_context.error = exception
@@ -77,12 +79,7 @@ class FlaskWrapper(BaseWrapper):
 
         @wraps(original_func)
         def wrapper(*args, **kwargs):
-            try:
-                from flask import request
-            except:
-                return original_func(*args, **kwargs)
-
-            if getattr(request, '_thundra_wrapped', False):
+            if request is None or getattr(request, '_thundra_wrapped', False):
                 return original_func(*args, **kwargs)
             setattr(request, '_thundra_wrapped', True)
             try:
