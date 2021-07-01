01c16876b2ef42a835a471fa505b050dde76f3e6
gokhansimsek <gokhansimsek@gokhansimseks-MacBook-Pro.local>
2021-07-01 16:34:02 +0300

    Readme file has been changed w.r.t repo name.

--- layer/deploy_layer.sh
+++ layer/deploy_layer.sh
@@ -5,7 +5,7 @@ BUCKET_PREFIX=$1
 LAYER_NAME_SUFFIX=$2
 
 REGIONS=( "ap-northeast-1" "ap-northeast-2" "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ca-central-1" "eu-central-1" "eu-north-1" "eu-west-1" "eu-west-2" "eu-west-3" "sa-east-1" "us-east-1" "us-east-2" "us-west-1" "us-west-2" )
-LAYER_NAME_BASE="thundra-python-layer"
+LAYER_NAME_BASE="thundra-lambda-python-layer"
 LAYER_NAME="$LAYER_NAME_BASE"
 
 if [[ ! -z "$LAYER_NAME_SUFFIX" ]]; then
@@ -30,7 +30,7 @@ echo "Zip completed."
  for REGION in "${REGIONS[@]}"
  do
     ARTIFACT_BUCKET=$BUCKET_PREFIX-$REGION
-    ARTIFACT_OBJECT=layers/python/thundra-agent-layer-$VERSION.zip
+    ARTIFACT_OBJECT=layers/python/thundra-agent-lambda-layer-$VERSION.zip
 
     echo "Uploading '$LAYER_NAME.zip' at $ARTIFACT_BUCKET with key $ARTIFACT_OBJECT"
     
