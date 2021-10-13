91e3ec131d71ec7c2e04a0065a3efcef29259020
gokhan721 <simsek.gokhan721@gmail.com>
2021-10-13 12:25:18 +0300

    comment out botocore

--- thundra/integrations/modules/botocore.py
+++ thundra/integrations/modules/botocore.py
@@ -33,19 +33,20 @@ def _wrapper(wrapped, instance, args, kwargs):
 
 
 def patch():
-    if not ConfigProvider.get(config_names.THUNDRA_TRACE_INTEGRATIONS_AWS_DISABLE):
-        wrapt.wrap_function_wrapper(
-            'botocore.client',
-            'BaseClient._make_api_call',
-            _wrapper
-        )
-    if not ConfigProvider.get(config_names.THUNDRA_TRACE_INTEGRATIONS_HTTP_DISABLE):
-        try:
-            wrapt.wrap_function_wrapper(
-                'botocore.vendored.requests',
-                'Session.send',
-                request_wrapper
-            )
-        except Exception:
-            # Vendored version of requests is removed from botocore
-            pass
+    pass
+    # if not ConfigProvider.get(config_names.THUNDRA_TRACE_INTEGRATIONS_AWS_DISABLE):
+    #     wrapt.wrap_function_wrapper(
+    #         'botocore.client',
+    #         'BaseClient._make_api_call',
+    #         _wrapper
+    #     )
+    # if not ConfigProvider.get(config_names.THUNDRA_TRACE_INTEGRATIONS_HTTP_DISABLE):
+    #     try:
+    #         wrapt.wrap_function_wrapper(
+    #             'botocore.vendored.requests',
+    #             'Session.send',
+    #             request_wrapper
+    #         )
+    #     except Exception:
+    #         # Vendored version of requests is removed from botocore
+    #         pass
