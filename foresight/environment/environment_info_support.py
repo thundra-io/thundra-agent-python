925117a8761c2359fe2d0486ac722058962dc1c8
gokhan721 <simsek.gokhan721@gmail.com>
2021-11-01 13:42:59 +0300

    adding additional debug console messages for foresight

--- foresight/environment/environment_info_support.py
+++ foresight/environment/environment_info_support.py
@@ -8,6 +8,7 @@ from foresight.environment.travisci.travisci_environment_info_provider import Tr
 from foresight.environment.circleci.circleci_environment_info_provider import CircleCIEnvironmentInfoProvider
 from foresight.environment.bitbucket.bitbucket_environment_info_provider import BitbucketEnvironmentInfoProvider
 from foresight.test_runner_tags import TestRunnerTags
+from foresight.utils.generic_utils import print_debug_message_to_console
 
 LOGGER = logging.getLogger(__name__)
 
@@ -35,19 +36,23 @@ class EnvironmentSupport:
             for key, clz in cls.ENVIRONMENTS_VARS.items():
                 LOGGER.debug("Current key, clz: {}, {}".format(key,clz))
                 if os.getenv(key):
-                    cls.environment_info = clz.build_env_info()
-                    LOGGER.debug("Environment info: {}".format(cls.environment_info.to_json()))
-                    LOGGER.debug("Founded key and class: {}, {}".format(key, clz))
-                    break
+                    ei = clz.build_env_info()
+                    if ei != {}: 
+                        cls.environment_info = ei
+                        print_debug_message_to_console("Environment info: {}".format(cls.environment_info.to_json()))
+                        print_debug_message_to_console("Founded key and class: {}, {}".format(key, clz))
+                        break
             if cls.environment_info == None:
                 if GitHelper.get_repo_url():
-                    LOGGER.debug("GitHelper environment info: {}".format(GitHelper.get_repo_url()))
+                    print_debug_message_to_console("Couldn't find any ci envrionment! Trying .git file...")
                     cls.environment_info = GitEnvironmentInfoProvider.build_env_info()
-                else:
-                    LOGGER.debug("Couldn't find .git file!")
+                    print_debug_message_to_console("Environment info: {}".format(cls.environment_info.to_json()))
+                else:                    
+                    print_debug_message_to_console("Couldn't find .git file!")
         except Exception as err:
             LOGGER.error("Environment Support environment_info could not set: {}".format(err))
             cls.environment_info = {}
+            print_debug_message_to_console("environment_info is None!")
             pass
 
 
@@ -58,13 +63,17 @@ class EnvironmentSupport:
         Args:
             obj (ThundraSpan): Span or invocation data
         """
-        if cls.environment_info:
-            span.set_tag(TestRunnerTags.TEST_ENVIRONMENT, cls.environment_info.environment)
-            span.set_tag(TestRunnerTags.SOURCE_CODE_REPO_URL, cls.environment_info.repo_url)
-            span.set_tag(TestRunnerTags.SOURCE_CODE_REPO_NAME, cls.environment_info.repo_name)
-            span.set_tag(TestRunnerTags.SOURCE_CODE_BRANCH, cls.environment_info.branch)
-            span.set_tag(TestRunnerTags.SOURCE_CODE_COMMIT_HASH, cls.environment_info.commit_hash)
-            span.set_tag(TestRunnerTags.SOURCE_CODE_COMMIT_MESSAGE, cls.environment_info.commit_message)
+        try:
+            if cls.environment_info != {}:
+                span.set_tag(TestRunnerTags.TEST_ENVIRONMENT, cls.environment_info.environment)
+                span.set_tag(TestRunnerTags.SOURCE_CODE_REPO_URL, cls.environment_info.repo_url)
+                span.set_tag(TestRunnerTags.SOURCE_CODE_REPO_NAME, cls.environment_info.repo_name)
+                span.set_tag(TestRunnerTags.SOURCE_CODE_BRANCH, cls.environment_info.branch)
+                span.set_tag(TestRunnerTags.SOURCE_CODE_COMMIT_HASH, cls.environment_info.commit_hash)
+                span.set_tag(TestRunnerTags.SOURCE_CODE_COMMIT_MESSAGE, cls.environment_info.commit_message)
+        except Exception as e:
+            LOGGER.error("Couldn't set environment info for span tags.")
+            pass
 
 
     @classmethod
@@ -74,10 +83,14 @@ class EnvironmentSupport:
         Args:
             obj (invocation): Span or invocation data
         """
-        if cls.environment_info:
-            invocation_data[TestRunnerTags.TEST_ENVIRONMENT] =  cls.environment_info.environment
-            invocation_data[TestRunnerTags.SOURCE_CODE_REPO_URL] =  cls.environment_info.repo_url
-            invocation_data[TestRunnerTags.SOURCE_CODE_REPO_NAME] =  cls.environment_info.repo_name
-            invocation_data[TestRunnerTags.SOURCE_CODE_BRANCH] =  cls.environment_info.branch
-            invocation_data[TestRunnerTags.SOURCE_CODE_COMMIT_HASH] =  cls.environment_info.commit_hash
-            invocation_data[TestRunnerTags.SOURCE_CODE_COMMIT_MESSAGE] =  cls.environment_info.commit_message
+        try:
+            if cls.environment_info != {}:
+                invocation_data[TestRunnerTags.TEST_ENVIRONMENT] =  cls.environment_info.environment
+                invocation_data[TestRunnerTags.SOURCE_CODE_REPO_URL] =  cls.environment_info.repo_url
+                invocation_data[TestRunnerTags.SOURCE_CODE_REPO_NAME] =  cls.environment_info.repo_name
+                invocation_data[TestRunnerTags.SOURCE_CODE_BRANCH] =  cls.environment_info.branch
+                invocation_data[TestRunnerTags.SOURCE_CODE_COMMIT_HASH] =  cls.environment_info.commit_hash
+                invocation_data[TestRunnerTags.SOURCE_CODE_COMMIT_MESSAGE] =  cls.environment_info.commit_message
+        except Exception as e:
+            LOGGER.error("Couldn't set environment info for invocation tags.")
+            pass
--- foresight/utils/generic_utils.py
+++ foresight/utils/generic_utils.py
@@ -7,3 +7,11 @@ def current_milli_time():
 
 def create_uuid4():
     return str(uuid.uuid4())
+
+
+def print_debug_message_to_console(msg):
+    from thundra.plugins.log.thundra_logger import debug_logger
+    from thundra.config.config_provider import ConfigProvider
+    from thundra.config import config_names
+    if ConfigProvider.get(config_names.THUNDRA_DEBUG_ENABLE):
+        debug_logger("[THUNDRA] " + msg)
