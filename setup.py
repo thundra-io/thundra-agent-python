5f82c4a625c1b6b06ca1561a0161e1ba17980da7
gokhan721 <simsek.gokhan721@gmail.com>
2021-11-06 16:47:24 +0300

    adding pympler to setup

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name='thundra',
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
       packages=find_packages(exclude=('tests', 'tests.*',)),
       install_requires=['requests>=2.16.0', 'opentracing>=2.0', 'wrapt>=1.10.11', 'simplejson', 'enum-compat',
-                        'jsonpickle==1.3', 'websocket-client', 'python-dateutil'],
+                        'jsonpickle==1.3', 'websocket-client', 'python-dateutil', 'pympler'],
       zip_safe=True,
       classifiers=[
           "Development Status :: 5 - Production/Stable",
