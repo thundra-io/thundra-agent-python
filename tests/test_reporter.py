5fda2b3e7ea2f7cfaa0bc4f38a65067464ddbf47
Erdoğan Yeşil <eyesil@catchpoint.com>
2023-06-08 00:41:52 +0300

    test code fixes

--- tests/test_reporter.py
+++ tests/test_reporter.py
@@ -30,37 +30,6 @@ def test_send_report_to_url(mock_requests, mock_report):
 
 
 @mock.patch('thundra.reporter.requests')
-def test_send_report_to_url_composite(mock_requests, mock_report, mock_invocation_report):
-    ConfigProvider.set(config_names.THUNDRA_REPORT_REST_BASEURL, 'different_url/api')
-    ConfigProvider.set(config_names.THUNDRA_REPORT_REST_COMPOSITE_ENABLE, 'true')
-    ConfigProvider.set(config_names.THUNDRA_REPORT_REST_COMPOSITE_BATCH_SIZE, '1')
-
-    test_session = mock_requests.Session()
-    reporter = Reporter('api key', session=test_session)
-
-    responses = reporter.send_reports([mock_invocation_report, mock_report])
-    assert reporter.session.post.call_count == 2
-
-    reporter.session.post.return_value.status_code = 200
-    for response in responses:
-        assert response.status_code == 200
-
-
-@mock.patch('thundra.reporter.requests')
-def test_send_report_to_url_async(mock_requests, mock_report):
-    ConfigProvider.set(config_names.THUNDRA_REPORT_REST_BASEURL, 'different_url/api')
-    ConfigProvider.set(config_names.THUNDRA_REPORT_CLOUDWATCH_ENABLE, 'true')
-
-    test_session = mock_requests.Session()
-    reporter = Reporter('api key', session=test_session)
-
-    responses = reporter.send_reports([mock_report])
-
-    reporter.session.post.assert_not_called()
-    assert responses == []
-
-
-@mock.patch('thundra.reporter.requests')
 def test_send_report(mock_requests, mock_invocation_report):
     test_session = mock_requests.Session()
     reporter = Reporter('unauthorized api key', session=test_session)
@@ -104,20 +73,3 @@ def test_prepare_report_json_batch(mock_report):
 
     reports = json.loads(batched_reports[0])
     assert len(reports) == 1
-
-
-def test_prepare_composite_report_json(mock_report, mock_invocation_report):
-    ConfigProvider.set(config_names.THUNDRA_REPORT_REST_COMPOSITE_BATCH_SIZE, '2')
-    reporter = Reporter('api key')
-    batched_reports = reporter.prepare_composite_report_json([mock_invocation_report, mock_report, mock_report])
-    composite_report = json.loads(batched_reports[0])
-
-    assert composite_report["type"] == "Composite"
-    assert composite_report["apiKey"] == "api key"
-    assert len(composite_report["data"]["allMonitoringData"]) == 2
-
-    composite_report = json.loads(batched_reports[1])
-
-    assert composite_report["type"] == "Composite"
-    assert composite_report["apiKey"] == "api key"
-    assert len(composite_report["data"]["allMonitoringData"]) == 1
